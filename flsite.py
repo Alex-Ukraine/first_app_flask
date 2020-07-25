18a52ac6a92a90331669f135322a766c8cb587cf
Alex-Ukraine <win.city18@gmail.com>
2020-07-26 01:53:20 +0300

    Flask #5: Подключение внешних ресурсов и работа с формами

--- flsite.py
+++ flsite.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-menu = ["Установка", "Первое приложение", "Обратная связь"]
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"}]
 
 # создаётся контекст запроса
 @app.route("/")
@@ -12,17 +14,22 @@ def index():
 
 @app.route("/about")
 def about():
-    print(url_for('about'))
     return render_template('about.html', title="О сайте", menu=menu)
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        print(request.form)
+    return render_template('contact.html', title="Обратная связь", menu=menu)
 
 @app.route("/profile/<username>")
 def profile(username):
     return f"Пользователь: {username}"
 
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('about'))
-    print(url_for('profile', username="selfedu"))
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('about'))
+#     print(url_for('profile', username="selfedu"))
 
-# if __name__ == "__main__":
-#    app.run(debug=True)
+if __name__ == "__main__":
+   app.run(debug=True)
