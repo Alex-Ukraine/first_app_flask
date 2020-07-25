d7be4e210172fd4cfb874275ed01d627aea672a6
Alex-Ukraine <win.city18@gmail.com>
2020-07-26 00:43:13 +0300

    Функция url_for и переменные URL-адреса

--- flsite.py
+++ flsite.py
@@ -1,16 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 menu = ["Установка", "Первое приложение", "Обратная связь"]
 
+# создаётся контекст запроса
 @app.route("/")
 def index():
+    print(url_for('index'))
     return render_template('index.html', menu=menu)
 
 @app.route("/about")
 def about():
+    print(url_for('about'))
     return render_template('about.html', title="О сайте", menu=menu)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Пользователь: {username}"
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('about'))
+    print(url_for('profile', username="selfedu"))
+
+# if __name__ == "__main__":
+#    app.run(debug=True)
