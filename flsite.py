86c51d998246c532c318c882398f450cddbf0e9e
Alex-Ukraine <win.city18@gmail.com>
2020-07-29 19:40:30 +0300

    Flask #14 Регистрация пользователей и шифрование паролей

--- flsite.py
+++ flsite.py
@@ -1,30 +1,108 @@
-from flask import Flask, render_template, make_response, url_for, redirect, request, session
-import datetime
+import sqlite3
+import os
+from flask import Flask, render_template, request, g, flash, abort, redirect, url_for
+from FDataBase import FDataBase
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+# конфигурация
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = 'sdfgsdfdfyhjdtyhnsfbsrtbndrnt'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '2abbb6cbe9bf31ad2459b1e51783b7f741f589b0'
-#app.permanent_session_lifetime Время жизни сессии 31 день
-app.permanent_session_lifetime = datetime.timedelta(days=10)
+app.config.from_object(__name__)
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_db():
+    """Вспомогательная функция для создания таблиц БД"""
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    '''Соединение с БД, если оно ещё не установлено'''
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+dbase = None
+@app.before_request
+def before_request():
+    '''Установление соединения с БД перед выполнением запроса'''
+    global dbase
+    db = get_db()
+    dbase = FDataBase(db)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    '''Закрываем соединение с БД, если оно было установлено'''
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
 
 @app.route("/")
 def index():
-    if 'visits' in session:
-        session['visits'] = session.get('visits') + 1 #обновление данных сессии
-    else:
-        session['visits'] = 1 #запись данных в сессию
-    return f"<h1>Main Page</h1><p>Число просмотров: {session['visits']}"
+    return render_template('index.html', menu=dbase.getMenu(), posts=dbase.getPostsAnonce())
 
-data = [1,2,3,4]
-@app.route("/session")
-def session_data():
-    session.permanent = True
-    if 'data' not in session:
-        session['data'] = data
-    else:
-        session['data'][1] += 1
-        session.modified = True
 
-    return f"<p>session['data']: {session['data']}"
+@app.route("/add_post", methods=["POST", "GET"])
+def addPost():
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'], request.form['url'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+    return render_template('add_post.html', menu=dbase.getMenu(), title="Добавление статьи")
+
+
+@app.route("/post/<alias>")
+def showPost(alias):
+    title, post = dbase.getPost(alias)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    return render_template("login.html", menu=dbase.getMenu(), title="Авторизация")
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['email']) > 4 \
+            and len(request.form['psw']) > 4 and request.form['psw'] == request.form['psw2']:
+            hash = generate_password_hash(request.form['psw'])
+            res = dbase.addUser(request.form['name'], request.form['email'], hash)
+            if res:
+                flash('Вы успешно зарегистрированы', category='success')
+                return redirect(url_for('login'))
+            else:
+                flash('Ошибка при добавлении в БД', category='error')
+        else:
+            flash('Неверно указаны поля', category='error')
+
+    return render_template('register.html', menu=dbase.getMenu(), title="Регистрация")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
